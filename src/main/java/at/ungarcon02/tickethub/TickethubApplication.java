ef0f38cc4f2861b11601a0942bb717adb28c1292
Etienne Pelletier <etiennepelletier0@gmail.com>
2026-02-06 00:55:53 +0100

    Initial commit

---
+++ src/main/java/at/ungarcon02/tickethub/TickethubApplication.java
@@ -0,0 +1,13 @@
+package at.ungarcon02.tickethub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TickethubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TickethubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/ungarcon02/tickethub/TickethubApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ungarcon02.tickethub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TickethubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

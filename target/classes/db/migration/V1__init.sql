bae4ffb0fe0aaf98518b6476925812af0bc6f3ba
Etienne Pelletier <etiennepelletier0@gmail.com>
2026-02-06 01:28:09 +0100

    Add Postgres and Flyway baseline

---
+++ target/classes/db/migration/V1__init.sql
@@ -0,0 +1,7 @@
+create table app_user (
+    id bigserial primary key,
+    email varchar(255) not null unique ,
+    password_hash varchar(255) not null ,
+    role varchar(255) not null ,
+    created_at timestamptz not null default now()
+);
